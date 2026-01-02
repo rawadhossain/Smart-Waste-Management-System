ce8d3718207fbddc47e19a7ed7bc59ab37e599a6
RX-kabir <156705525+RX-kabir@users.noreply.github.com>
2026-01-02 10:20:00 +0600

    added auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartWaste.Web.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        [HttpGet("/Auth")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Access";
+            return View();
+        }
+    }
+}
