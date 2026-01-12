f9002051f06d1ea81b44223f09cdea17f38128ed
adnan <adnankenway@gmail.com>
2026-01-12 10:10:00 +0600

    updated bin list code

---
+++ Models/BinListRowVm.cs
@@ -0,0 +1,21 @@
+﻿namespace SmartWaste.Web.Models
+{
+    public class BinListRowVm
+    {
+        public int BinId { get; set; }
+        public int ZoneId { get; set; }
+        public string ZoneName { get; set; } = "";
+        public string Location { get; set; } = "";
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public int CapacityLiters { get; set; }
+        public string WasteType { get; set; } = "";
+
+        public byte BinStatusId { get; set; }
+        public string StatusName { get; set; } = "";
+
+        public int? LatestFillLevelPercent { get; set; }
+        public DateTime? LastReadingTime { get; set; }
+        public decimal? LatestTemperature { get; set; }
+    }
+}
