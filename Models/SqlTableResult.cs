80cf2aefdb5fa72247de0191075d0aab1ed8e961
adnan <adnankenway@gmail.com>
2026-01-14 15:25:00 +0600

    table results sql

---
+++ Models/SqlTableResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartWaste.Web.Models
+{
+    public class SqlTableResult
+    {
+        public string Title { get; set; } = "";
+        public List<string> Columns { get; set; } = new();
+        public List<List<string>> Rows { get; set; } = new();
+    }
+}
