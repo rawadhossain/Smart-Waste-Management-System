eb222e3cbd95150fc551b7167c96e38222326087
Monajir <monajirwantswar@gmail.com>
2026-02-08 11:20:00 +0600

    feat(models): add AddReadingVM for public bin reading submission

---
+++ Models/AddReadingVm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartWaste.Web.Models
+{
+    public class AddReadingVm
+    {
+        [Required]
+        public int BinId { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int FillLevelPercent { get; set; }
+
+        public decimal? Temperature { get; set; }
+
+        public DateTime? ReadingTime { get; set; }
+
+        // dropdown support
+        public List<(string Value, string Text)> Bins { get; set; } = new();
+    }
+}
